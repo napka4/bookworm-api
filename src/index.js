238a1ffea3524d9d4403e252269694ba6072b030
Taenys <nanshee4@gmail.com>
2018-12-05 15:47:09 +0100

    commit auth

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import bodyPaser from 'body-parser';
+import dotenv from 'dotenv';
+import Promise from 'bluebird';
+
+import auth from './routes/auth';
+
+dotenv.config();
 
 const app = express();
+app.use(bodyPaser.json());
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URL, { useMongoClient: true });
 
-app.post('/api/auth', (req, res) => {
-    res.status(400).json({ errors: { global: "Mauvais logs !"}});
-});
+app.use('/api/auth', auth);
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-})
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(8080, () => console.log ('Marche sur le local serveur 8080'));
